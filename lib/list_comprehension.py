37f753d09ab7ba3bc7904dbc95a18005b298bfe4
Joseph <josephshonko99@gmail.com>
2024-05-28 13:39:19 +0300

    commiting comprehention

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # pass
+  return [num for num in num_list if num % 2 == 0]
+
+print(return_evens([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) 
+
+print(return_evens([1, 3, 5, 7, 9]))
+
 
 def make_exclamation(sentence_list):
-    pass
+ # pass
+ return [sentence + '!' for sentence in sentence_list]
+
+    print(make_exclamation(["Hello", "I like computers", "Live long and prosper"]))
+
+def test_return_exclamation_list(self):
+    return "I like computers"
+    print(test_return_exclamation_list)
